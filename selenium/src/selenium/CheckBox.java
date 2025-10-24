f96e40fd6167636c056f04d46bab2c515eece05e
MEDA <MEDA@DESKTOP-CF8Q1LN>
2025-10-24 12:59:13 +0530

    skkkkkkkkkkkk

--- selenium/src/selenium/CheckBox.java
+++ selenium/src/selenium/CheckBox.java
@@ -25,7 +25,7 @@ public class CheckBox {
 				System.out.println("checkbox is selected");
 				
 			} else {
-				System.out.println("checkbox is not selectedddd");
+				System.out.println("checkbox is not selecteddd");
 				
 			}
 		}
